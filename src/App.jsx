357abed6ef1c931176d114767c98336e1173527d
shamil <shamilmohd418@gmail.com>
2024-01-29 15:18:04 +0530

    efdf

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import './App.css'
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import app from './config';
 import { useEffect, useState } from 'react';
+import { doc, setDoc } from "firebase/firestore"; 
+
 
 function App() {
-
+  const [datas,setData]=useState({
+    name:"",
+    age:""
+  })
   const [user,setUser]=useState([])
+  const db = getFirestore(app);
+
+  const data=(e)=>{
+    setData((pre)=>({...pre,[e.target.name]:e.target.value}))
+    console.log(datas);
+  }
 
   const getData=async()=>{
-    const db = getFirestore(app);
+   
     const data = await getDocs(collection(db, 'user'));
     const userData = [];
     data.forEach(doc => {
@@ -21,17 +32,32 @@ function App() {
   useEffect(()=>{
     console.log(user);
   },[user])
+
+  const addData = async () => {
+    try {
+      await setDoc(doc(db, 'user', datas.name), datas);
+      console.log('Document added successfully!');
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+  };
+
   return (
     <>
-      <button onClick={getData}>Click Here</button>
-      {
+      <input type="text" name="name" id="" placeholder='Name' onChange={data}/>
+      <input type="text" name="age" id="" placeholder='Age' onChange={data}/>
+      <button onClick={addData}>Add Data</button>
+      <button onClick={getData}>Privew</button>
+<div className="main">
+{
         user.map((data,index)=>
-        <div key={index}>
-          <p>{data.name}</p>
-          <p>{data.age}</p>
+        <div key={index} className="card">
+        <div><p>Name : {data.name}</p></div>
+          <div><p>Age : {data.age}</p></div>
         </div>
         )
       }
+</div>
     </>
   )
 }
